676568c9b966a743b09616546f4df73f2c13b7e8
Samuel Souza <silva.samueldev@gmail.com>
2025-11-22 11:54:06 -0300

    :pencil2: fix: correção do mapeamento do bibliotecario

--- src/main/java/com/grupo04/Biblioteca/controllers/BibliotecarioController.java
+++ src/main/java/com/grupo04/Biblioteca/controllers/BibliotecarioController.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/bibliotecario")
+@RequestMapping("/bibliotecarios")
 public class BibliotecarioController {
     @Autowired
     public BibliotecarioRepository repository;
