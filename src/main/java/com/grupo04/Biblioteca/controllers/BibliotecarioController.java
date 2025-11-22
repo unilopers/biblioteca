51e2bd3ea2c6511315e4a388a4aff78824990e58
Samuel Souza <silva.samueldev@gmail.com>
2025-11-22 14:49:44 -0300

    :memo: docs: definindo a tag da controller do bibliotecario

--- src/main/java/com/grupo04/Biblioteca/controllers/BibliotecarioController.java
+++ src/main/java/com/grupo04/Biblioteca/controllers/BibliotecarioController.java
@@ -3,6 +3,8 @@ package com.grupo04.Biblioteca.controllers;
 import com.grupo04.Biblioteca.dto.BibliotecarioDTO;
 import com.grupo04.Biblioteca.models.BibliotecarioModel;
 import com.grupo04.Biblioteca.repository.BibliotecarioRepository;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jdk.jfr.Name;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +15,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/bibliotecarios")
+@RequestMapping(value = "/bibliotecarios")
+@Tag(name = "Bibliotecario")
 public class BibliotecarioController {
     @Autowired
     public BibliotecarioRepository repository;
