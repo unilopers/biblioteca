c72935581b9097887110f4380e42b5b2e80cb68e
Bugres Gustavo <bugresgustavo@MacBook-Pro-de-Gustavo.local>
2025-11-24 13:24:43 -0300

    :sparkles: feat: adicionar atributos id e nome na entidade Autor

--- src/main/java/com/grupo04/Biblioteca/models/autor.java
+++ src/main/java/com/grupo04/Biblioteca/models/autor.java
@@ -2,3 +2,12 @@ package com.grupo04.Biblioteca.models;
 public class autor {
     
 }
+@Entity
+@Table(name = "autor")
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name = "cd_autor")
+private Integer id;
+
+@Column(name = "nm_autor", nullable = false, length = 50)
+private String nome;
