153b3b3e667bc6e962665a734a755c2a7c6a1b47
TecGustaF <168040734+TecGustaF@users.noreply.github.com>
2025-11-24 14:07:50 -0300

    Add AutorController with CRUD methods
    adicionando crud parcial

--- src/main/java/com/grupo04/Biblioteca/controllers/AutorController.java
+++ src/main/java/com/grupo04/Biblioteca/controllers/AutorController.java
@@ -1,5 +1,34 @@
 package com.grupo04.Biblioteca.controllers;
 
+import com.grupo04.Biblioteca.models.Autor;
+import com.grupo04.Biblioteca.repository.AutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/autores")
 public class AutorController {
-    
 }
+
+ @Autowired
+    private AutorRepository autorRepository;
+
+    @PostMapping
+    public Autor criarAutor(@RequestBody Autor autor) {
+        autor.setDataCadastro(LocalDateTime.now());
+        return autorRepository.save(autor);
+    }
+
+    @GetMapping
+    public List<Autor> listarAutores() {
+        return autorRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Autor> buscarPorId(@PathVariable Integer id) {
+        return autorRepository.findById(id);
+    }
+
