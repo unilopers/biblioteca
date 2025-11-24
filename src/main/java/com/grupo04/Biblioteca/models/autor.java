3679338b30adb02b6efaa88174b3ba834efeb17d
TecGustaF <168040734+TecGustaF@users.noreply.github.com>
2025-11-24 13:50:26 -0300

    corrigindo estrutura e adicionando construtor

--- src/main/java/com/grupo04/Biblioteca/models/autor.java
+++ src/main/java/com/grupo04/Biblioteca/models/autor.java
@@ -1,13 +1,30 @@
 package com.grupo04.Biblioteca.models;
-public class autor {
-    
-}
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "autor")
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-@Column(name = "cd_autor")
-private Integer id;
+public class Autor {
 
-@Column(name = "nm_autor", nullable = false, length = 50)
-private String nome; 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cd_autor")
+    private Integer id;
+
+    @Column(name = "nm_autor", nullable = false, length = 50)
+    private String nome;
+
+    @Column(name = "dt_cadastro", nullable = false)
+    private LocalDateTime dataCadastro;
+
+    @Column(name = "sn_ativo", nullable = false, length = 1)
+    private String ativo;
+
+ public Autor() {}
+
+     public Autor(String nome, LocalDateTime dataCadastro, String ativo) {
+        this.nome = nome;
+        this.dataCadastro = dataCadastro;
+        this.ativo = ativo;
+    }
