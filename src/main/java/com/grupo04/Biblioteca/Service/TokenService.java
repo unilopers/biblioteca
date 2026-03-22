712cb6a55a2a812d635e9c97fe80c14701b4dbc2
Samuel Souza <silva.samueldev@gmail.com>
2026-03-22 18:25:39 -0300

    :bug: fix: validacao do token

--- src/main/java/com/grupo04/Biblioteca/Service/TokenService.java
+++ src/main/java/com/grupo04/Biblioteca/Service/TokenService.java
@@ -1,12 +1,14 @@
 package com.grupo04.Biblioteca.Service;
 
 
-
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Service
@@ -15,12 +17,16 @@ public class TokenService {
     private String SECRET_KEY;
 
     public String gerarToken(String usuario) {
-
+        SecretKey key = new SecretKeySpec(
+                SECRET_KEY.getBytes(StandardCharsets.UTF_8),
+                "HmacSHA256"
+        );
         return Jwts.builder()
                 .setSubject(usuario) // usuário dono do token
+                .claim("scope", "USER")
                 .setIssuedAt(new Date()) // data de criação
                 .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // expira em 24h
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .signWith(key)
                 .compact();
     }
 }
--- src/main/java/com/grupo04/Biblioteca/config/SecurityConfig.java
+++ src/main/java/com/grupo04/Biblioteca/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.grupo04.Biblioteca.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -9,17 +10,23 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/auth").permitAll().requestMatchers("/bibliotecarios").permitAll()
-                    .anyRequest().authenticated()
+                        .requestMatchers("/auth").permitAll().requestMatchers("/bibliotecarios").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(Customizer.withDefaults()));
@@ -28,8 +35,12 @@ public class SecurityConfig {
 
     @Bean
     public JwtDecoder jwtDecoder() {
+        SecretKey key = new SecretKeySpec(
+                SECRET_KEY.getBytes(StandardCharsets.UTF_8),
+                "HmacSHA256"
+        );
         return NimbusJwtDecoder
-                .withJwkSetUri("http://localhost:8080/realms/meu-realm/protocol/openid-connect/certs")
+                .withSecretKey(key)
                 .build();
     }
 
